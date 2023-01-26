d2ae112f1dfa3711c703b81604eca501a644f85e
xxaksed <aksed.336@mail.ru>
2023-01-26 15:35:07 +0300

    newSave

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,45 +1,128 @@
 import React, { useEffect, useState } from "react";
 import Tile from "./Tile";
+import uuid from 'react-uuid';
+import update from 'react-addons-update';
 
+// const mockObj = {
+//     t: {
+//         a: {
+//             yolo: [1, {
+//                 kek: true
+//             }]
+//         }
+//     }
+// }
 
+// const newMockObj = update(mockObj, {
+//     t: {
+//         a: {
+//             yolo: [, {
+//                 kek:{
+//                 $set: true
+//                 }
+//             }]
+//         }
+//     }
+// },
+
+//     { $push: false }
+// )
+console.log(mockObj)
 function NewGame() {
-    const [hod, setHod] = useState(0)
-    const [game, setGame] = useState(
+    const [hod, setHod] = useState(1)
+    const [gameEnd, setGameEnd] = useState(false)
+    const [defaultGame, setDefaultGame] = useState(
         [
             [0, 0, 0],
             [0, 0, 0],
             [0, 0, 0],
-        ])
-        console.log(game)
-        
-        let gameChange = game
+        ]);
+    const [game, setGame] = useState(defaultGame);
 
-        function onClickTiktaktoe(row, item){
 
-            gameChange[] == "1"
+    function checkWin() {
 
-            console.log(gameChange)
+        if (game[0][0] == 1 && game[0][1] == 1 && game[0][2] == 1 ||
+            game[1][0] == 1 && game[1][1] == 1 && game[1][2] == 1 ||
+            game[2][0] == 1 && game[2][1] == 1 && game[2][2] == 1 ||
+            game[0][0] == 1 && game[1][0] == 1 && game[2][0] == 1 ||
+            game[0][1] == 1 && game[1][1] == 1 && game[2][1] == 1 ||
+            game[0][2] == 1 && game[1][2] == 1 && game[2][2] == 1 ||
+            game[0][0] == 1 && game[1][1] == 1 && game[2][2] == 1 ||
+            game[0][2] == 1 && game[1][1] == 1 && game[2][0] == 1) {
+            setGameEnd(true)
+            return <div>'player "X" won'</div>
         }
-        
+        if (game[0][0] == 2 && game[0][1] == 2 && game[0][2] == 2 ||
+            game[1][0] == 2 && game[1][1] == 2 && game[1][2] == 2 ||
+            game[2][0] == 2 && game[2][1] == 2 && game[2][2] == 2 ||
+            game[0][0] == 2 && game[1][0] == 2 && game[2][0] == 2 ||
+            game[0][1] == 2 && game[1][1] == 2 && game[2][1] == 2 ||
+            game[0][2] == 2 && game[1][2] == 2 && game[2][2] == 2 ||
+            game[0][0] == 2 && game[1][1] == 2 && game[2][2] == 2 ||
+            game[0][2] == 2 && game[1][1] == 2 && game[2][0] == 2) {
+            setGameEnd(true)
+            return <div>'player "O" won'</div>
+        }
+    }
+
+    function onRowClickHandler(gameIndex, rowIndex) {
+
+
+
+        setHod(hod + 1)
+        let localhod = hod + 1
+
+        setGame(update(game,
+            {
+                [gameIndex]: {
+                    $splice: [[[rowIndex], 1, localhod % 2 === 0 ? 1 : 2]]
+                }
+            })
+        )
+
+    }
+
+    function clickOnButton() {
+        setHod(1)
+        setGame(defaultGame)
+        setGameEnd(false)
+    }
+
+    useEffect(() => {
+        checkWin()
+    }, [hod]);
 
     return (
         <div className="Board">
-            {game.map((row, index) => {
+            {game.map((row, gameIndex) => {
                 return (
-                    <div className = 'layer' key={Math.random()*9999+1}>{row.map((item, index) => {
+                    <div className="layer" key={uuid()}>{row.map((value, rowIndex) => {
                         return (
-                            <Tile key={Math.random()*9999+1}  
-                            onClickFunc={()=>onClickTiktaktoe(row, item)}
-                            value={item}
-                            >
-                            </Tile>)
+                            <Tile
+                                key={uuid()}
+                                value={game[gameIndex][rowIndex]}
+
+                                onClickFunc={!gameEnd ? () => onRowClickHandler(gameIndex, rowIndex) : null} />
+                        )
                     })}
                     </div>
                 )
             })}
+
+            <div>Game status: {hod}</div>
+            <div><button onClick={clickOnButton}>reset game</button></div>
+            <div>{checkWin()}</div>
+
+            <div>{JSON.stringify(mockObj)}</div>
+            <div><button>KEK FALSE</button></div>
+            <div></div>
+
+
         </div>
     )
 
 }
+
 
 export default NewGame;
--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,6 +1,19 @@
 import React, { useState } from "react";
 
-function Tile({onClickFunc, value}) {
+function Tile({onClickFunc, value, }) {   
+
+if(value > 0){
+    onClickFunc = undefined
+}
+if(value == 0){
+    value = ""
+}
+if(value == 1){
+    value = "X"
+}
+if(value == 2){
+    value = "O"
+}
 
     return (
         <div className="box" onClick={onClickFunc}>
